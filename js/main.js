510f0d458301b4906b2105c631c82bfac06c77aa
OzziWaterdo <oswaldo-aguado@outlook.com>
2023-06-25 21:53:26 -0400

    Adicion de Formulario

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+//Formulario
+function enviarMensaje() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var asunto = document.getElementById("asunto").value;
+    var mensaje = document.getElementById("mensaje").value;
+  
+    window.alert("Se ha enviado tu mensaje.");
+    console.log("Nombre: " + nombre);
+    console.log("Apellido: " + apellido);
+    console.log("Asunto: " + asunto);
+    console.log("Mensaje: " + mensaje);
+  }
