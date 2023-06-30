96e072777500e30e45dd9c5d5a6166df532611ec
OzziWaterdo <oswaldo-aguado@outlook.com>
2023-06-29 21:44:52 -0400

    Finalgit add .!

--- js/main.js
+++ js/main.js
@@ -1,18 +1,56 @@
 
 //Formulario
 function enviarMensaje() {
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var asunto = document.getElementById("asunto").value;
-    var mensaje = document.getElementById("mensaje").value;
-  
-    window.alert("Se ha enviado tu mensaje.");
-    console.log("Nombre: " + nombre);
-    console.log("Apellido: " + apellido);
-    console.log("Correo: " + correo);
-    console.log("Asunto: " + asunto);
-    console.log("Mensaje: " + mensaje);
-  }
+  var nombre = document.getElementById("nombre").value;
+  var apellido = document.getElementById("apellido").value;
+  var asunto = document.getElementById("asunto").value;
+  var mensaje = document.getElementById("mensaje").value;
 
- 
- 
+  window.alert("Se ha enviado tu mensaje.");
+  console.log("Nombre: " + nombre);
+  console.log("Apellido: " + apellido);
+  console.log("Correo: " + correo);
+  console.log("Asunto: " + asunto);
+  console.log("Mensaje: " + mensaje);
+}
+
+
+//Modo Noche
+
+document.body.style="background-color: var(--bs-dark);transition: 0.5s; color: var(--text-color)"
+const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
+
+let theme = "dark";
+  const root = document.querySelector(":root");
+  const container = document.getElementsByClassName("theme-container")[0];
+  const themeIcon = document.getElementById("theme-icon");
+  container.addEventListener("click", setTheme);
+  function setTheme() {
+    theme === "dark" ? (setLight(), theme = "light") : (setDark(), theme = "dark")
+  }
+  function setLight() {
+    root.style.setProperty(
+      "--bs-dark",
+      "linear-gradient(318.32deg, #c3d1e4 0%, #dde7f3 55%, #d4e0ed 100%)"
+    );
+    root.style.setProperty("--text-color", "#000");
+    container.classList.remove("shadow-dark");
+    setTimeout(() => {
+      container.classList.add("shadow-light");
+      themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    themeIcon.src = sun;
+  }
+  function setDark() {
+    root.style.setProperty("--bs-dark", "#212529");
+    root.style.setProperty("--text-color", "#fff");
+    container.classList.remove("shadow-light");
+    setTimeout(() => {
+      container.classList.add("shadow-dark");
+      themeIcon.classList.remove("change");
+    }, 300);
+    themeIcon.classList.add("change");
+    themeIcon.src = moon;
+  }
